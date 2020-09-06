7660164ab233f00eec330aa110bfa00c4ab42ed4
plungerman <plungerman@gmail.com>
2020-09-06 11:39:10 -0500

    updates to area and capacity data models for tracking capacity over time

--- djindahaus/core/models.py
+++ djindahaus/core/models.py
@@ -13,26 +13,40 @@ class Area(models.Model):
 
     created_at = models.DateTimeField("Date Created", auto_now_add=True)
     updated_at = models.DateTimeField("Date Updated", auto_now=True)
+    rf_domain = models.CharField(
+        "RF Domain Name",
+        max_length=128,
+    )
     name = models.CharField("Friendly Name", max_length=128)
-    rf = models.CharField("RF Domain Name", max_length=128, null=True, null=True)
     parent = models.ForeignKey(
+        'self',
         on_delete=models.CASCADE,
         blank=True,
         null=True,
         related_name='children',
     )
     capacity = models.IntegerField()
+    rank = models.IntegerField(
+        verbose_name="Ranking",
+        default=0,
+        help_text="A number that determines this object's position in a list.",
+    )
     active = models.BooleanField(default=True, verbose_name="Active?")
+    access_points = models.TextField(
+        null=True,
+        blank=True,
+        help_text="Comma separated list of MAC addresses. Format: B8-50-01-3A-CB-C6",
+    )
     tags = TaggableManager(blank=True)
 
     class Meta:
-        ordering = ('-created_at',)
+        """Attributes about the data model and admin options."""
+
+        ordering = ['-created_at']
 
     def __str__(self):
         """Default data for display."""
-        return "{0}, {1}".format(
-            self.created_by.last_name, self.created_by.first_name
-        )
+        return self.name
 
 
 class Capacity(models.Model):
@@ -40,7 +54,7 @@ class Capacity(models.Model):
 
     area = models.ForeignKey(
         Area,
-        related_name='capacity',
+        related_name='occupied',
         editable=False,
         on_delete=models.CASCADE,
     )
@@ -50,14 +64,20 @@ class Capacity(models.Model):
         null=True,
         blank=True,
         default=0,
-        help_text="A number that determines this object's position in a list.",
+    )
+    capacity = models.IntegerField(
+        verbose_name="Capacity Number",
+        null=True,
+        blank=True,
+        default=0,
+        help_text="Capacity number at the time of creation.",
     )
     status = models.BooleanField(default=True, verbose_name="Active?")
 
     class Meta:
         """Attributes about the data model and admin options."""
 
-        ordering = ['created_at-']
+        ordering = ['-created_at']
 
     def __str__(self):
         """Default data for display."""
